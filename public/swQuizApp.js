f7ff6444fc256774764d7c0ecf8ed4159fd9ee59
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-08-26 04:52:53 +0500

    Update swQuizApp.js

--- public/swQuizApp.js
+++ public/swQuizApp.js
@@ -16,7 +16,7 @@
     '/static/media/photo-1.c11f97a3.jpg',    
     '/favicon.ico',
     '/',
-    'max-age=31536000' ,
+    '/0f31201287e0e3f7e87c86ed26f2a492', 
     'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
 
    ]
